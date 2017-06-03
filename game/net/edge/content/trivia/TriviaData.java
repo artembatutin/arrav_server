1ae0b73a614838fe4a7852cce6556d499bd931de
Stan <stan.dupforyourself@hotmail.com>
2017-06-03 20:38:19 +0200

    Forgot 2 files for the trivia.

---
+++ game/net/edge/content/trivia/TriviaData.java
@@ -0,0 +1,68 @@
+package net.edge.content.trivia;
+
+import com.google.common.collect.ImmutableSet;
+import net.edge.util.rand.RandomUtils;
+
+/**
+ * @author <a href="http://www.rune-server.org/members/stand+up/">Stand Up</a>
+ * @since 3-6-2017.
+ */
+public enum TriviaData {
+    _1("Who are the owners of Edgeville?", "Avro and Stan", "Avro & Stan", "Stan & Avro", "Stan and Avro"),
+    _2("Name one game developer.", "Avro", "Stan"),
+    _3("What is the maximum combat level?", "138"),
+    _4("Howmuch coins does a starterpack in Edgeville contain?", "200k", "200000", "200_000"),
+    _5("Who is the shop owner of the Blood money store?", "Hazelmere"),
+    _6("Who is the shop owner of the Edge tokens store?", "Party Pete"),
+    _7("How many Hitpoints do Sharks heal?", "20", "Twenty", "200", "Two-hundred", "Two hundred"),
+    _8("What Magic level is needed to cast Ice Barrage?", "94"),
+    _9("What is the highest tier defender?", "dragon", "dragon defender"),
+    _10("What is the required Attack level needed to wield the Abyssal whip?", "70", "Seventy"),
+    _11("What is the required Strength level needed to wield the Tzhaar-ket-om?", "60", "Sixty"),
+    _12("What is the required Ranged level needed to wield Red chinchompas?", "55", "Fifty-five", "Fifty five"),
+    _13("How many charges can an amulet of glory hold?", "6", "Six"),
+    _14("What herb is required to make a Prayer potion?", "Ranarr"),
+    _15("What boss drops the Elysian sigil?", "Corporeal Beast"),
+    _16("The Amulet of fury is created using which gem?", "Onyx"),
+    _17("How many types of godswords are there?", "4", "Four"),
+    _18("What is the Prayer requirement needed to wield the Elysian spirit shield?", "75", "Seventy-five", "Seventy five"),
+    _19("What ore can be mined at level 70 Mining?", "Adamantite", "Adamantite ore", "Adamamant ore"),
+    _20("Which godsword has the strongest special attack in PvP?", "Armadyl godsword"),
+    _21("Which godswords special attack can heal you?", "Saradomin godsword"),
+    _22("Which godswords special attack drains your opponents stats?", "Bandos godsword"),
+    _23("Which godswords special attack freezes your enemy?", "Zamorak godsword"),
+    ;
+
+    /**
+     * Caches our enum values.
+     */
+    public static final TriviaData[] VALUES = values();
+
+    /**
+     * The question for this trivia.
+     */
+    public final String question;
+
+    /**
+     * The possible answers for this trivia
+     */
+    public final String[] answers;
+
+    /**
+     * Constructs a new {@link TriviaData}.
+     * @param question  {@link #question}.
+     * @param answers   {@link #answers}.
+     */
+    TriviaData(String question, String... answers) {
+        this.question = question;
+        this.answers = answers;
+    }
+
+    /**
+     * Gets a random question.
+     * @return a random question.
+     */
+    public static TriviaData random() {
+        return RandomUtils.random(VALUES);
+    }
+}
---
+++ game/net/edge/content/trivia/TriviaEntry.java
@@ -0,0 +1,113 @@
+package net.edge.content.trivia;
+
+import net.edge.world.World;
+import net.edge.game.GameConstants;
+import net.edge.util.TextUtils;
+import net.edge.util.rand.RandomUtils;
+import net.edge.world.node.entity.player.Player;
+import net.edge.world.node.item.Item;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author <a href="http://www.rune-server.org/members/stand+up/">Stand Up</a>
+ * @since 3-6-2017.
+ */
+public final class TriviaEntry {
+
+    /**
+     * The current trivia question being asked.
+     */
+    public TriviaData current = TriviaData.random();
+
+    /**
+     * The list of attempted answers.
+     */
+    public final List<String> attempted_answers = new ArrayList<>();
+
+    /**
+     * Sends a message to the entire world asking the trivia question.
+     */
+    public void ask() {
+        World.get().message("@red@[Trivia Bot]: @blu@" + current.question);
+    }
+
+    /**
+     * Sends a reminder to the world.
+     */
+    public void reminder() {
+        World.get().message("@red@[Trivia Bot]: @blu@The last question hasn't been answered yet!");
+        ask();
+    }
+
+    /**
+     * The trivia question that should be send to the player on login.
+     * @param player    the player to send the question to.
+     */
+    public void onLogin(Player player) {
+        player.message("@red@[Trivia Bot]: @blu@" + current.question);
+    }
+    /**
+     * Attempts to answer the current trivia question.
+     * @param player    the player attempting to answer.
+     * @param answer    the answer that the player has submit.
+     */
+    public void answer(Player player, String answer) {
+        if(current == null) {
+            player.message("@red@[Trivia Bot]: @blu@The last question has been answered or has expired already.");
+            return;
+        }
+
+        if (Arrays.stream(GameConstants.BAD_STRINGS).anyMatch(answer::contains)) {
+            player.message("@red@[Trivia Bot]: @blu@You think you're funny, don't you? Guess what? You ain't.");
+            return;
+        }
+
+        if (Arrays.stream(current.answers).anyMatch(a -> a.equalsIgnoreCase(answer))) {
+            answered(player, answer);
+            return;
+        }
+
+        if(!attempted_answers.contains(answer)) {
+            attempted_answers.add(answer);
+        }
+
+        if (RandomUtils.inclusive(10) == 0) {
+            player.forceChat("Whoops! I just entered a wrong trivia answer!");
+        }
+
+        player.message("@red@[Trivia Bot]: @blu@Sorry, the answer you have entered is incorrect! Try again!");
+    }
+
+    /**
+     * The functionality that occurs when a player successfully answers a trivia question.
+     * @param player    the player whom answered the trivia question.
+     * @param answer    the answer that was correct.
+     */
+    private void answered(Player player, String answer) {
+        World.get().message("@red@[Trivia Bot]: @bla@" + player.getFormatUsername() + "@blu@ has answered the question correctly! @red@Answer: " + answer + "@blu@.");
+
+        if (!attempted_answers.isEmpty()) {
+            List<String> attemptedAnswers = attempted_answers.stream().filter(a -> Arrays.stream(GameConstants.BAD_STRINGS).noneMatch(a::contains)).collect(Collectors.toList());
+            World.get().message("@red@[Trivia Bot]: @blu@Attempted answers: @red@" + attemptedAnswers.toString() + "@blu@!");
+        }
+
+        int amount = RandomUtils.inclusive(10_000, 30_000);
+
+        player.getBank().add(0, new Item(995, amount));
+        player.message("@red@[Trivia Bot]: @blu@" + TextUtils.formatPrice(amount) + " coins were added into your bank.");
+        reset();
+    }
+
+    /**
+     * Resets the current question and it's attempted answers.
+     */
+    public void reset() {
+        current = null;
+        attempted_answers.clear();
+    }
+}
+
