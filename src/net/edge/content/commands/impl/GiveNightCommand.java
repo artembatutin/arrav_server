35be96e6ef0c2d908f4ffe98b5ce3d3071f43851
Artem Batutin <artembatutin@gmail.com>
2017-06-07 03:11:13 -0400

    Added a night give command.

---
+++ src/net/edge/content/commands/impl/GiveNightCommand.java
@@ -0,0 +1,21 @@
+package net.edge.content.commands.impl;
+
+import net.edge.content.commands.Command;
+import net.edge.content.commands.CommandSignature;
+import net.edge.world.World;
+import net.edge.world.node.entity.player.Player;
+import net.edge.world.node.entity.player.assets.Rights;
+
+@CommandSignature(alias = {"night"}, rights = {Rights.DEVELOPER, Rights.ADMINISTRATOR}, syntax = "Use this command as ::night player")
+public final class GiveNightCommand implements Command {
+	
+	@Override
+	public void execute(Player player, String[] cmd, String command) throws Exception {
+		Player night = World.get().getPlayer(cmd[1].replaceAll("_", " ")).orElse(null);
+		if(night == null)
+			return;
+		night.setNight(1);
+		night.message(player.getFormatUsername() + " set you to nightmare mode.");
+	}
+	
+}
