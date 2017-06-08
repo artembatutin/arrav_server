6fdce0e781d5c2ad53e342003258c9686d49a910
Artem Batutin <artembatutin@gmail.com>
2017-06-08 04:47:25 -0400

    Commands changes.

--- src/net/edge/content/commands/impl/EventsCommand.java
+++ src/net/edge/content/commands/impl/EventsCommand.java
@@ -1,17 +1,19 @@
 package net.edge.content.commands.impl;
 
+import net.edge.Server;
 import net.edge.content.commands.Command;
 import net.edge.content.commands.CommandSignature;
+import net.edge.net.database.connection.use.Donating;
 import net.edge.world.World;
 import net.edge.world.node.entity.player.Player;
 import net.edge.world.node.entity.player.assets.Rights;
 
-@CommandSignature(alias = {"core"}, rights = {Rights.DEVELOPER}, syntax = "Use this command as ::core")
-public final class CoreCommand implements Command {
+@CommandSignature(alias = {"event"}, rights = {Rights.DEVELOPER}, syntax = "Use this command as ::event")
+public final class EventsCommand implements Command {
 
 	@Override
 	public void execute(Player player, String[] cmd, String command) throws Exception {
-		player.message("Took " + World.millis + "ms on sync.");
+		Server.loadEvents();
 	}
 
 }
