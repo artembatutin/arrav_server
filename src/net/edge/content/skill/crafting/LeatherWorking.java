d241e2970e1a219929afc68aef19fc8241614417
Stan <stan.dupforyourself@hotmail.com>
2017-06-17 13:56:14 +0200

    Hard leather can now be crafted.

--- src/net/edge/content/skill/crafting/LeatherWorking.java
+++ src/net/edge/content/skill/crafting/LeatherWorking.java
@@ -13,6 +13,7 @@ import net.edge.world.node.item.Item;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -47,6 +48,11 @@ public final class LeatherWorking extends ProducingSkillAction {
 	 * A constant defining the leather item.
 	 */
 	private static final Item LEATHER = new Item(1741);
+
+	/**
+	 * A constant defining the hard leather item.
+	 */
+	private static final Item HARD_LEATHER = new Item(1743);
 	
 	/**
 	 * A constant defining the needle item.
@@ -75,8 +81,17 @@ public final class LeatherWorking extends ProducingSkillAction {
 		if(data == null) {
 			return false;
 		}
-		
-		LeatherWorking crafting = new LeatherWorking(player, data, data.amount);
+
+		if(data.amount == -1) {
+			player.getMessages().sendEnterAmount("How many would you like to craft?", s -> () -> {
+				LeatherWorking leatherWorking = new LeatherWorking(player, data, Integer.parseInt(s));
+				leatherWorking.start();
+			});
+			return true;
+		}
+
+		int amount = data.amount == -2 ? player.getInventory().computeAmountForId(HARD_LEATHER.getId()) : data.amount;
+		LeatherWorking crafting = new LeatherWorking(player, data, amount);
 		crafting.start();
 		return true;
 	}
@@ -88,6 +103,13 @@ public final class LeatherWorking extends ProducingSkillAction {
 	 * @return {@code true} if the interface was opened, {@code false} otherwise.
 	 */
 	public static boolean openInterface(Player player, Item itemUsed, Item usedOn) {
+		if(itemUsed.getId() == NEEDLE.getId() && usedOn.getId() == HARD_LEATHER.getId() || itemUsed.getId() == HARD_LEATHER.getId() && usedOn.getId() == NEEDLE.getId()) {
+			player.getMessages().sendString("\\n\\n\\n\\n\\n" + HARD_LEATHER.getDefinition().getName(), 2799);
+			player.getMessages().sendItemModelOnInterface(1746, 150, 1131);
+			player.getMessages().sendString("How many would you like to make?", 2800);
+			player.getMessages().sendChatInterface(4429);
+			return true;
+		}
 		if(itemUsed.getId() == NEEDLE.getId() && usedOn.getId() == LEATHER.getId() || itemUsed.getId() == LEATHER.getId() && usedOn.getId() == NEEDLE.getId()) {
 			player.getMessages().sendInterface(2311);
 			return true;
@@ -112,7 +134,8 @@ public final class LeatherWorking extends ProducingSkillAction {
 	
 	@Override
 	public Optional<Item[]> removeItem() {
-		return Optional.of(new Item[]{LEATHER, THREAD});
+		Item item = data.produced.getId() == 1131 ? HARD_LEATHER : LEATHER;
+		return Optional.of(new Item[]{item, THREAD});
 	}
 	
 	@Override
@@ -191,7 +214,12 @@ public final class LeatherWorking extends ProducingSkillAction {
 		
 		LEATHER_COIF(33202, 1169, 38, 37, 1),
 		LEATHER_COIF5(33201, LEATHER_COIF, 5),
-		LEATHER_COIF10(33200, LEATHER_COIF, 10);
+		LEATHER_COIF10(33200, LEATHER_COIF, 10),
+
+		HARD_LEATHER_BODY(10239, 1131, 28, 35, 1),
+		HARD_LEATHER_BODY5(10238, HARD_LEATHER_BODY, 5),
+		HARD_LEATHER_BODYX(6212, HARD_LEATHER_BODY, -1),
+		HARD_LEATHER_BODYALL(6211, HARD_LEATHER_BODY, -2);
 		
 		/**
 		 * Caches our enum values.
