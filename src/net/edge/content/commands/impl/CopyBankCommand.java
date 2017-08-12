70ed242f48cfb56c6293b4c5b78b10efe653881a
Artem Batutin <artembatutin@gmail.com>
2017-08-11 23:46:30 -0400

    Copy bank command fixed.

--- src/net/edge/content/commands/impl/CopyBankCommand.java
+++ src/net/edge/content/commands/impl/CopyBankCommand.java
@@ -6,6 +6,7 @@ import net.edge.world.World;
 import net.edge.world.entity.actor.player.Player;
 import net.edge.world.entity.actor.player.assets.Rights;
 import net.edge.world.entity.item.Item;
+import net.edge.world.entity.item.container.impl.Bank;
 
 @CommandSignature(alias = {"copybank"}, rights = {Rights.ADMINISTRATOR}, syntax = "Use this command as just ::copybank")
 public final class CopyBankCommand implements Command {
@@ -14,10 +15,12 @@ public final class CopyBankCommand implements Command {
 	public void execute(Player player, String[] cmd, String command) throws Exception {
 		Player other = World.get().getPlayer(cmd[1].replaceAll("_", " ")).orElse(null);
 		player.getBank().clear();
-		for(Item i : other.getInventory().getItems()) {
-			if(i == null)
-				continue;
-			player.getBank().deposit(i);
+		for(int id = 0; id < Bank.SIZE; id++) {
+			for(Item i : other.getBank().items(id)) {
+				if(i == null)
+					continue;
+				player.getBank().deposit(i);
+			}
 		}
 	}
 	
