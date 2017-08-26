a1176945fac65effb07b54615271293bc9fac4b1
chex_mix <mmaksoudian@gmail.com>
2017-08-26 05:16:36 -0700

    Removed dfs code from the dragonfire strategy class, should be as a listener

--- src/net/edge/content/combat/strategy/npc/impl/DragonfireStrategy.java
+++ src/net/edge/content/combat/strategy/npc/impl/DragonfireStrategy.java
@@ -4,29 +4,14 @@ import net.edge.content.combat.CombatProjectileDefinition;
 import net.edge.content.combat.CombatUtil;
 import net.edge.content.combat.attack.FightType;
 import net.edge.content.combat.hit.CombatHit;
-import net.edge.content.combat.hit.Hit;
 import net.edge.content.combat.strategy.npc.NpcMagicStrategy;
 import net.edge.world.entity.actor.Actor;
 import net.edge.world.entity.actor.mob.Mob;
-import net.edge.world.entity.actor.player.Player;
-import net.edge.world.entity.item.container.impl.Equipment;
 
 public class DragonfireStrategy extends NpcMagicStrategy {
 
     public DragonfireStrategy(CombatProjectileDefinition projectileDefinition) {
         super(projectileDefinition);
-    }
-
-    @Override
-    public void attack(Mob attacker, Actor defender, Hit hit) {
-        super.attack(attacker, defender, hit);
-        if (defender.isPlayer()) {
-            Player player = defender.toPlayer();
-
-            if (player.getEquipment().getId(Equipment.SHIELD_SLOT) == 11283) {
-                // TODO: dragonfire shield effects to defender here
-            }
-        }
     }
 
     @Override
