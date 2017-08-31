40a0d42ffb20b58153515141efdd0791d1b005bd
Dave van Bochove <dave_bochove@hotmail.com>
2017-08-31 16:25:28 +0200

    Everlasting Poison Fix, Tokhaar-kal untradable, skull command

--- src/net/edge/world/entity/actor/combat/effect/impl/CombatPoisonEffect.java
+++ src/net/edge/world/entity/actor/combat/effect/impl/CombatPoisonEffect.java
@@ -59,16 +59,25 @@ public final class CombatPoisonEffect extends CombatEffect {
 	
 	@Override
 	public void process(Actor t) {
-		amount--;
-		t.damage(new Hit(t.getPoisonDamage().get() * 10, Hitsplat.POISON, HitIcon.NONE));
-		if(amount == 0) {
-			amount = 4;
-			int val = t.getPoisonDamage().decrementAndGet();
-			
-			if(val < 1) {
-				t.getPoisonDamage().set(0);//clear poison.
+		if (t.getPoisonDamage().get() <= 0) {
+			if(t.isPlayer()) {
+				t.toPlayer().out(new SendConfig(174, 0));
 			}
+			this.removeOn(t);
+			return;
 		}
+		t.damage(new Hit(t.getPoisonDamage().get() * 10, Hitsplat.POISON, HitIcon.NONE));
+		t.getPoisonDamage().decrementAndGet();
+//		amount--;
+//		t.damage(new Hit(t.getPoisonDamage().get() * 10, Hitsplat.POISON, HitIcon.NONE));
+//		if(amount == 0) {
+//			amount = 4;
+//			int val = t.getPoisonDamage().decrementAndGet();
+//
+//			if(val < 1) {
+//				t.getPoisonDamage().set(0);//clear poison.
+//			}
+//		}
 	}
 	
 	@Override
